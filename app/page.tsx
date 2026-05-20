db609a44661ffcbfdf6d80e9eb5f527b8e20ec02
jandryramon99-developer <jandryramon99@gmail.com>
2026-05-19 23:27:56 -0500

    Actualización (variable entorno)

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,20 @@ import CTASection from "@/components/landing/CTASection";
 import Navbar from "@/components/landing/NavBarSection";
 import FeaturedCourses from "@/components/landing/FeaturedCourses";  
 
+const BACKEND_URL = !!process.env.VERCEL_ENV ? process.env.NEXT_PUBLIC_BACKEND_URL : "http://localhost:4000";
+
+
 async function getCourses() {
-  const res = await fetch("http://localhost:4000/courses", { cache: "no-store" });
+  const res = await fetch(BACKEND_URL + "/courses", { cache: "no-store" });
   console.log("Código de respuesta de la API:", res.status); 
   if (!res.ok) throw new Error("Error al obtener los cursos");
   return res.json();
+  
 }
-
 
 export default async function Home() {
   const courses = await getCourses();
-
+  
   return (
     <main className="min-h-screen bg-black text-white overflow-hidden">
 
