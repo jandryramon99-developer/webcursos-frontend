d71e5830e8db1b5aed8ab8af95b7ec9e93037eed
jandryramon99-developer <jandryramon99@gmail.com>
2026-05-25 18:48:01 -0500

    Actualización correcciones para servidor

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,14 @@
 //import Image from "next/image";
-import { env }
-from "@/lib/env";
+
 import Footer from "@/components/landing/Footer";
 import HeroSection from "@/components/landing/HeroSection";
 import BenefitsSection from "@/components/landing/BenefitsSection";
 import CTASection from "@/components/landing/CTASection";
 import Navbar from "@/components/landing/NavBarSection";
 import FeaturedCourses from "@/components/landing/FeaturedCourses";  
+import { getCourses } from "@/services/course.service";
 
 
-
-
-async function getCourses() {
-  const res = await fetch(env.API_URL + "/api/courses", { cache: "no-store" });
-  console.log("Código de respuesta de la API:", res.status); 
-  if (!res.ok) throw new Error("Error al obtener los cursos");
-  return res.json();
-  
-}
 
 export default async function Home() {
   const courses = await getCourses();
